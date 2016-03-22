ef96b432c703746adad280027c8ac16aa7eef72c
dreylopez <dreylopez@danielcastelao.org>
2016-03-22 18:58:26 +0100

    Creo los archivos __init__.py y __openerp__.py

---
+++ modulo-odoo/TemaOdoo/__init__.py
@@ -0,0 +1,14 @@
+{
+  'name':'Tema Odoo',
+  'description': ' Odoo',
+  'version':'1.0',
+  'author':'David Rey',
+
+ 'data': [
+  'views/layout.xml',
+  'views/pages.xml',
+  'views/vacaciones.xml',
+  'views/assets.xml'
+],
+  'category': 'Theme/Creative',
+  'depends': ['website'],

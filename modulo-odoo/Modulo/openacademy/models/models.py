c181e54d117846c2e0bbd99b6ab173aff9a587f3
dreylopez <dreylopez@danielcastelao.org>
2016-03-25 19:54:32 +0100

    Modificamos y editamos algunos ficheros de los menus y datos

--- modulo-odoo/Modulo/openacademy/models/models.py
+++ modulo-odoo/Modulo/openacademy/models/models.py
@@ -2,14 +2,24 @@
 
 from openerp import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
+class Course(models.Model):
+    _name = 'openacademy.course'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string="Titulo", required=True)
+    description = fields.Text()
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsable", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sesiones")
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Curso", required=True)
